01de1127f6f0b5597c2c904de6937f3ee3e3f724
gdserafini <gdinizserafini@gmail.com>
2023-01-18 17:05:05 -0300

    user schema add

---
+++ server/src/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    background: {
+        type: String,
+        required: true
+    }
+});
+
+export const User = mongoose.Schema('User', UserSchema);
