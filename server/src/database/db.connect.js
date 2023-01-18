4934e644886d55c47041bc4739bf0f65e6aa247e
gdserafini <gdinizserafini@gmail.com>
2023-01-18 19:39:39 -0300

    refat

--- server/src/database/db.connect.js
+++ server/src/database/db.connect.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 
-export const connectDB = function(){
-    console.log('Connecting to data base...');
+const stringConnectMongo = 'mongodb+srv://root:%40Password123@' +
+    'cluster0.49niqow.mongodb.net/?retryWrites=true&w=majority';
 
+const setUseMongo = {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}
+
+const messageConnecting = function(){
+    console.log('Connecting to data base...');
+};
+
+const setStrict = function(){
     mongoose.set('strictQuery', false);
+};
+
+export const connectDB = function(){
+    messageConnecting();
+    setStrict();
     
-    mongoose.connect(
-        'mongodb+srv://root:%40Password123@cluster0.49niqow.mongodb.net/?retryWrites=true&w=majority',
-        {useNewUrlParser: true, useUnifiedTopology: true}
-    )
-    .then(() => console.log('Connected.'))
-    .catch(error => console.log(error));
+    mongoose.connect(stringConnectMongo, setUseMongo)
+        .then(() => console.log('Connected.'))
+        .catch(error => console.log(error));
 
 };
