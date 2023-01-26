1b64d73dadddba6440837813d9f9cc302e071c0f
gdserafini <gdinizserafini@gmail.com>
2023-01-26 15:05:27 -0300

    middlewares updated

--- server/src/middlewares/global.middleware.js
+++ server/src/middlewares/global.middleware.js
@@ -2,35 +2,37 @@
 export const validId = function(req, res, next){
     const id = req.params.id;
 
-    if(!id) return {
+    if(!id) return res.json({
         statusCode: 400,
         message: 'Missing id.'
-    };
+    });
 
     if(isNaN(parseInt(id)) ||
         parseInt(id) <= 0) {
 
-            return {
+            return res.json({
                 statusCode: 400,
                 message: 'Invalid id.'
-            };
+            });
     };
 
     next();
 };
 
 export const validBody = function(req, res, next){
-    if(!req.body) return {
+    if(!req.body) return res.json({
         statusCode: 400,
         message: 'Missing data.'
-    };
+    });
 
     const params = Object.keys(req.body);
 
     if(params.length === 0){
-        return {
+        return res.json({
             statusCode: 400,
             message: 'The body is empty.'
-        };
-    }
+        });
+    };
+
+    next();
 };
