9a1ba7b1a510169dc40473fa8322d1a61567616b
gdserafini <gdinizserafini@gmail.com>
2023-01-26 20:51:31 -0300

    mogan logger config

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { BootstrapDB } from './src/database/prismaMysql.js';
 import usersRouter from './src/routers/user.router.js';
 import authRouter from './src/routers/auth.router.js';
 import dotenv from 'dotenv';
+import logger from 'morgan';
 
 dotenv.config();
 
@@ -12,14 +13,34 @@ const normalizePort = function(value){
     if(isNaN(port)) return value;
     if(port >= 0) return port;
     return false;
-}
+};
+
+const isDev = function(){
+    return process.env.NODE_ENV === 'dev';
+};
+
+const isProd = function(){
+    return process.env.NODE_ENV === 'prod';
+};
+
+const isCi = function(){
+    return process.env.NODE_ENV === 'ci';
+};
+
+const isStaging = function(){
+    return !isProd();
+};
 
 const app = express();
 const PORT = normalizePort(process.env.PORT || '3000');
+const MORGAN_FORMAT = isDev() ? 'dev' : 
+    `:remote-addr :method :url: HTTP/:http-version :status 
+    :res[content-length] :response-time ms :user-agent`;
 
 app.set('port', PORT);
 
 //BootstrapDB();
+app.use(logger(MORGAN_FORMAT));
 app.use(express.json());
 
 app.listen(PORT);
