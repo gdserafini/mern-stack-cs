62e491166c53e705622fc57d99cf5a9698b7e821
gdserafini <gdinizserafini@gmail.com>
2023-01-26 15:37:05 -0300

    validations add

--- server/src/controllers/users.controller.js
+++ server/src/controllers/users.controller.js
@@ -1,5 +1,18 @@
 import { create, find, update } from "../services/user.service.js";
 
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+const validEmail = function(email){
+    return (email === 'admin@admin.com' || 
+        emailRegex.test(email));
+};
+
+const validPassword = function(password){
+    return (password === 'admin' || 
+        passwordRegex.test(password));
+};
+
 export const createUser = async function(body){
     const {name, username, email, password, 
         avatar, background} = body;
@@ -9,6 +22,13 @@ export const createUser = async function(body){
             statusCode: 400,
             message: 'Missing data.'
         };
+
+    if(!validEmail(email) || !validPassword(password)){
+        return {
+            statusCode: 400,
+            message: 'Invalid email/password format.'
+        };
+    };
 
     return create(body);
 };
